59f8131b58c3b727a4a81adf91f49ea890ba8231
Emanoel Destruidor <Emanoel Destruidor>
2017-05-06 12:33:40 -0300

    updated

--- js/script.js
+++ js/script.js
@@ -8,20 +8,42 @@
 		this.addMusicToList = function (music) {
 			if (!music) return;
 
-			let li = document.createElement("li");
+			let li = document.createElement('li');
 			li.appendChild(document.createTextNode(music));
 			musicList.appendChild(li);
+
+			let a = document.createElement('button');
+			a.appendChild(document.createTextNode('x'));
+			a.className = 'rm';
+			a.id = 'rmFromList';
+
+			li.appendChild(a);
+
 			musicName.value = '';
+		}
+
+		this.rmMusicFromList = function (element) {
+			element.parentElement.remove();
 		}
 	}
 
-	var setList = new SetList();
+	let setList = new SetList();
+	let body = document.getElementsByTagName('body')[0];
 
-	setList.form.addEventListener('submit', function (e) {
-		e.preventDefault();
+	setList.form.addEventListener('submit', 
+		function (e) {
+			e.preventDefault();
 
-		setList.addMusicToList(setList.musicName.value);
-		setList.musicName.focus();
-	});
+			setList.addMusicToList(setList.musicName.value);
+			setList.musicName.focus();
+		});
+
+	body.addEventListener('click', 
+		function(event) {
+			if (event.target.id === 'rmFromList')
+				setList.rmMusicFromList(event.target);
+		});
+
+
 
 }());
